cf51655b6b357aab9a02e96b5f2d6c7dcdee1c47
bep <8226605+bepvte@users.noreply.github.com>
2023-03-23 10:42:06 -0400

    freeskip and no volume

--- src/main/java/com/jagrosh/jmusicbot/commands/dj/VolumeCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/dj/VolumeCmd.java
@@ -40,30 +40,7 @@ public class VolumeCmd extends DJCommand
     @Override
     public void doCommand(CommandEvent event)
     {
-        AudioHandler handler = (AudioHandler)event.getGuild().getAudioManager().getSendingHandler();
-        Settings settings = event.getClient().getSettingsFor(event.getGuild());
-        int volume = handler.getPlayer().getVolume();
-        if(event.getArgs().isEmpty())
-        {
-            event.reply(FormatUtil.volumeIcon(volume)+" Current volume is `"+volume+"`");
-        }
-        else
-        {
-            int nvolume;
-            try{
-                nvolume = Integer.parseInt(event.getArgs());
-            }catch(NumberFormatException e){
-                nvolume = -1;
-            }
-            if(nvolume<0 || nvolume>150)
-                event.reply(event.getClient().getError()+" Volume must be a valid integer between 0 and 150!");
-            else
-            {
-                handler.getPlayer().setVolume(nvolume);
-                settings.setVolume(nvolume);
-                event.reply(FormatUtil.volumeIcon(nvolume)+" Volume changed from `"+volume+"` to `"+nvolume+"`");
-            }
-        }
+    	event.reply("no");
     }
     
 }
